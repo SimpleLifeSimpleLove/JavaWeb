c31c9edd11fcbefd69ca1c5ecef1ecc578e5b6d8
SimpleLifeSimpleLove <1137247975@qq.com>
2020-11-05 21:23:56 +0800

    response request

---
+++ javaweb-02-servlet/request-01/src/main/java/com/wxx/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.wxx;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Date: 2020/11/5 20:41
+ * Content:
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 设置请求与相应的编码
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");  // 设置前端显示的数据类型
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String[] hobbies = req.getParameterValues("hobbies");
+
+        System.out.println("===============================");
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(Arrays.toString(hobbies));
+        System.out.println("===============================");
+
+        // 这里的 / 代表当前的Web应用
+//        this.getServletContext().getRequestDispatcher("/success.jsp").forward(req, resp);  // 请求转发
+//        resp.sendRedirect("/success.jsp");  // 重定向
+        req.getRequestDispatcher("/success.jsp").forward(req, resp);  // 请求转发
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ javaweb-02-servlet/request-01/src/main/webapp/index.jsp
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>登陆</title>
+</head>
+
+<body>
+
+<h1>登陆</h1>
+
+<div style="text-align: center">
+    <%--这里的表单表示的意思：以post方式提交表单，提交到我们的login请求--%>
+    <form action="${pageContext.request.contextPath}/login" method="post">
+        用户名：<input type="text" name="username"> <br>
+        密码：<input type="password" name="password"> <br>
+        爱好：
+        <input type="checkbox" name="hobbies" value="学习">学习
+        <input type="checkbox" name="hobbies" value="代码">代码
+        <input type="checkbox" name="hobbies" value="唱歌">唱歌
+        <input type="checkbox" name="hobbies" value="电影">电影 <br>
+
+        <input type="submit">
+    </form>
+</div>
+
+</body>
+</html>
